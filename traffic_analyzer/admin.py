29a20b388ea3fa4412b6e7a8cd851d9b1aaeaad1
lanzy-lanzy <gerlan.masterpay@gmail.com>
2024-12-10 07:26:26 +0800

    new file:   README.zip 	modified:   db.sqlite3 	new file:   media/videos/Unbelievably_busy_bicycle_crossing_in_Amsterdam_pqQSwQLDIK8_Jni4iiU.mp4 	modified:   traffic_analyzer/__pycache__/admin.cpython-38.pyc 	modified:   traffic_analyzer/admin.py

--- traffic_analyzer/admin.py
+++ traffic_analyzer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import VideoAnalysis, VehicleCount, DetectionZone
 
-# Register your models here.
+@admin.register(VideoAnalysis)
+class VideoAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status')
+    list_filter = ('status',)
+
+@admin.register(VehicleCount)
+class VehicleCountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vehicle_type', 'frame_number', 'confidence', 'speed')
+    list_filter = ('vehicle_type',)
+
+@admin.register(DetectionZone)
+class DetectionZoneAdmin(admin.ModelAdmin):
+    list_display = ['id']
